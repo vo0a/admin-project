d02f79eadc859c817fa43d18f350079d1c32f3a4
vo0a <vo0a195@gmail.com>
2019-12-02 22:20:23 +0900

    [ADD] 사용자 API DELETE

--- src/main/java/com/example/admin_project/controller/api/UserApiController.java
+++ src/main/java/com/example/admin_project/controller/api/UserApiController.java
@@ -40,6 +40,7 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
     @Override
     @DeleteMapping("{id}") // /api/user/{id}
     public Header delete(@PathVariable(name = "id") Long id) {
-        return null;
+        log.info("delete id : {}", id);
+        return userApiLogicService.delete(id);
     }
 }
